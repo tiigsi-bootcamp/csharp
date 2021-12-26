2ac0191f42343cbfef4c2f62bf1ed5fb742b2e78
Mohamed Ali <somcoder@outlook.com>
2021-12-26 17:14:51 +0300

    Day 1 and Day 2

---
+++ Day1/Program.cs
@@ -0,0 +1,25 @@
+﻿
+// Statically Typed Language.
+
+/*
+Line 1
+Line 2
+*/
+
+
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("C# is fun!");
+
+var variableName = "Some Value";
+
+float number1 = 10000.000000000000088885f;
+
+Console.WriteLine("Double: " + number1);
+
+Console.WriteLine("Variable is:" + variableName);
+
+
+// C#  10
+// .NET 6.0
---
+++ Day2/Program.cs
@@ -0,0 +1,26 @@
+﻿
+// Strings.
+
+var person = "   Jamila Hassan         ";
+
+person = person.TrimEnd();
+person = person.TrimStart();
+
+var name = "    Ahmed    Ali     ".Trim();
+
+var personName = "Naima     Ali ";
+personName = personName.Replace("Naima", "Anab");
+
+Console.WriteLine("Name: '" + person + "'");
+Console.WriteLine("Name: '" + name + "'");
+Console.WriteLine("Name: '" + personName + "'");
+
+var lyric = "The older I get, the harder I fall!";
+
+var result = lyric.Contains("older");
+Console.WriteLine("Contains 'elder':" + result);
+
+result = lyric.StartsWith("The");
+Console.WriteLine("Starts with 'The':" + result);
+
+Console.WriteLine("Total chars: " + lyric.Length);
